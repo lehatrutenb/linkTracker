416fa214afb1213fa2622b2925a722ce6928cc80
lehatr <lehatrutenb@gmail.com>
2026-05-28 11:56:38 +0300

    style: apply autoformat

--- scrapper/src/test/java/backend/academy/linktracker/scrapper/ScrapperIntegrationTest.java
+++ scrapper/src/test/java/backend/academy/linktracker/scrapper/ScrapperIntegrationTest.java
@@ -18,8 +18,6 @@ import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
